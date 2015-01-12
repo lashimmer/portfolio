ce55a278cbb2f25d6382c490fda2b564e04ed953
shanna wang <lashimmer@gmail.com>
2015-01-12 17:24:34 -0500

    challenge winter theme??

--- portfolio/done.sh
+++ portfolio/done.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-for f in public/scss/*.scss 
-do
+# for f in public/scss/*.scss 
+# do
 
-	filename=$(basename "$f")
-	filename="${filename%.*}"
+# 	filename=$(basename "$f")
+# 	filename="${filename%.*}"
 
-	if [[ $filename =~ ^_ ]]; then
-		continue
-	else
-		sass public/scss/$filename.scss public/css/$filename.css
-		#java -jar yuicompressor-2.4.8.jar ../$filename.css -o ../css/$filename.css
-	fi
+# 	if [[ $filename =~ ^_ ]]; then
+# 		continue
+# 	else
+# 		sass public/scss/$filename.scss public/css/$filename.css
+# 		
+# 		#java -jar yuicompressor-2.4.8.jar ../$filename.css -o ../css/$filename.css
+# 	fi
 
-	echo $filename.css converted
+# 	echo $filename.css converted
 
-done
+# done
+
+sass public/scss/challenge.scss public/css/challenge.css
+sass public/scss/challenge.scss app/views/css/challenge.css
+
 
 echo "Conversion done!"
-git add --all *
-git commit -m "/design update"
-git push origin master
+
+# git add --all *
+# git commit -m "/design update"
+# git push origin master
