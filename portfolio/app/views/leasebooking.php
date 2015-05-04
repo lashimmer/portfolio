d0e71339db9c1ca96d86228458a9d70d19779592
shanna wang <lashimmer@gmail.com>
2015-05-03 21:13:12 -0400

    responsive

--- portfolio/app/views/leasebooking.php
+++ portfolio/app/views/leasebooking.php
@@ -6,6 +6,8 @@
 <head>
 
 	<title>Flatbook</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="./css/flatbook-challenge/main.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js"></script>
